b1fab6918c6cebc2eb7269b59bdfd58e0561e295
Naomi Dennis <njdennis94@gmail.com>
2018-04-17 15:46:55 +0000

    automatically backed up by learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ActiveRecord::Base
-    validates :title, presence: true
-    validates :content, length: { minimum: 100 }
-    validates :inclusion, {with: %w(Fiction Non-Fiction)}
+  validates :name, presence: true
+  validates :email, uniqueness: true
+  validates :phone_number, length: { is: 10}
 end
