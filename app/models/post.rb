d80e04e83a43fa6b855d23f6f4b877b6d2e3bf6f
Naomi Dennis <njdennis94@gmail.com>
2018-04-17 15:46:22 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
     validates :name, presence: true
-    validates :email, uniquness: true
+    validates :email, uniqueness: true
     validates :phone_number, length: { is: 10}
 end
